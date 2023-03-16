28f6096b4e57e06a4ed20ea8e59b724874fe7555
devopsbyraham <125343344+devopsbyraham@users.noreply.github.com>
2023-03-16 07:57:07 +0530

    new version

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('** this is my version 2.0 :) **'); //write a response to the client
+  res.write('** this is my version 3.0 :) **'); //write a response to the client
   res.end(); //end the response
 }).listen(80); //the server object listens on port 80

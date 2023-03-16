36a14b695333a2122e41ac5ea4ac002bbc97a782
devopsbyraham <125343344+devopsbyraham@users.noreply.github.com>
2023-03-16 07:55:31 +0530

    new commits

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('** Hey my app was deployed :) **'); //write a response to the client
+  res.write('** this is my version 2.0 :) **'); //write a response to the client
   res.end(); //end the response
 }).listen(80); //the server object listens on port 80
